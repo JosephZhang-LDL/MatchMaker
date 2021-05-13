8162be521eb8e0214c0bde8ac14aa6afb9b6bec5
JosephZhang-LDL <joseph.h.zhang@gmail.com>
2021-05-13 17:34:44 -0400

    Reformat and reallocate files to the right areas

--- src/components/MyCrushes/CrushAdder.jsx
+++ src/components/MyCrushes/CrushAdder.jsx
@@ -5,14 +5,12 @@ import {
   Input,
   Text,
   HStack,
-  Flex,
   Button,
-  Avatar,
   SimpleGrid,
 } from "@chakra-ui/react";
 import { TabContainer, Row, Col } from "../general/Standard";
+import CrushBox from "./CrushBox";
 import { FiHeart } from "react-icons/fi";
-import { IoClose } from "react-icons/io5";
 
 const crushes = [
   {
@@ -65,47 +63,6 @@ const CrushAdder = (props) => {
         </SimpleGrid>
       </TabContainer>
     </Box>
-  );
-};
-
-const CrushBox = (props) => {
-  return (
-    <Flex
-      borderRadius="2px"
-      backgroundColor="gray.100"
-      width="100%"
-      px="4"
-      py="2"
-      justify="space-between"
-      flexDirection="row"
-      gap="12px"
-    >
-      <Flex>
-        <Col>
-          <Avatar src={props.profPic} />
-        </Col>
-        <Col ml="3">
-          <Text fontSize="18px" fontWeight="medium">
-            {props.name}
-          </Text>
-          <Text fontSize="14px" color="gray.600" fontWeight="light" mt="-1">
-            {props.username}
-          </Text>
-        </Col>
-      </Flex>
-      <Col alignSelf="center">
-        <Text
-          color="gray.400"
-          _hover={{
-            color: "red.500",
-            cursor: "pointer",
-            transition: ".25s ease",
-          }}
-        >
-          <IoClose size="24px" />
-        </Text>
-      </Col>
-    </Flex>
   );
 };
 
---
+++ src/components/MyCrushes/CrushBox.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import {
+  Text,
+  Flex,
+  Avatar,
+} from "@chakra-ui/react";
+import { Col } from "../general/Standard";
+import { IoClose } from "react-icons/io5";
+
+const CrushBox = (props) => {
+  return (
+    <Flex
+      borderRadius="2px"
+      backgroundColor="gray.100"
+      width="100%"
+      px="4"
+      py="2"
+      justify="space-between"
+      flexDirection="row"
+      gap="12px"
+    >
+      <Flex>
+        <Col>
+          <Avatar src={props.profPic} />
+        </Col>
+        <Col ml="3">
+          <Text fontSize="18px" fontWeight="medium">
+            {props.name}
+          </Text>
+          <Text fontSize="14px" color="gray.600" fontWeight="light" mt="-1">
+            {props.username}
+          </Text>
+        </Col>
+      </Flex>
+      <Col alignSelf="center">
+        <Text
+          color="gray.400"
+          _hover={{
+            color: "red.500",
+            cursor: "pointer",
+            transition: ".25s ease",
+          }}
+        >
+          <IoClose size="24px" />
+        </Text>
+      </Col>
+    </Flex>
+  );
+};
+
+export default CrushBox;
