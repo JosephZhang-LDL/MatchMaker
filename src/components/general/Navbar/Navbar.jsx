8beac2e301afc5727a4ed601ebed3c823670bdf0
JosephZhang-LDL <joseph.h.zhang@gmail.com>
2021-05-30 13:40:00 -0400

    Fixed navbar links

--- src/components/general/Navbar/Navbar.jsx
+++ src/components/general/Navbar/Navbar.jsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { Flex, Box, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 import MenuItem from "./MenuItem";
-import styled from "styled-components";
-import { TabContainer } from "../Standard";
 
 const Navbar = (props) => {
   return (
     <Flex py=".75em" px="5em" boxShadow="base" justify="space-between">
       <Text color="green.500" fontSize="28px" fontWeight="bold">
-        MatchMe
+        <Link to="/">MatchMe</Link>
       </Text>
       <Flex align="center">
         <MenuItem text="My Crushes" link="my-crushes" />
